1a787478fb996a5f0c552114ffa5b0246994af2a
io12 <io12@users.noreply.github.com>
2016-05-23 22:06:07 -0400

    changed inline functions to regular functions

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -1,5 +1,10 @@
 /* framebuffer.c */
 
+#include <ints.h>
+#include <string.h>
+
+#include "ioport.h"
+
 #include "framebuffer.h"
 
 // Initialize values for the framebuffer struct
@@ -9,3 +14,39 @@ fb_t fb = {
 	// position
 	0
 };
+
+// Move cursor to a new position
+void fb_mov(fb_pos_t pos) {
+	io_out(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+	io_out(FB_DATA_PORT, pos >> 8);
+	io_out(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+	io_out(FB_DATA_PORT, pos & 0x00FF);
+}
+
+// Creates a framebuffer cell based on two colors and an ascii value
+fb_char_t fb_mkchar(fb_color_t fb_bgcolor, fb_color_t fb_fgcolor, uint8_t ascii) {
+	return ((uint8_t) fb_bgcolor << 12) | ((uint8_t) fb_fgcolor << 8) | ascii;
+}
+
+// Write a character to the framebuffer
+void fb_putchar(uint8_t ascii) {
+	fb.p[fb.pos] = fb_mkchar(FB_BLACK, FB_WHITE, ascii);
+	fb.pos++;
+	fb_mov(fb.pos);
+}
+
+// Write a string to the framebuffer
+void fb_puts(const char* str) {
+	size_t i;
+	size_t length = strlen(str);
+
+	for (i = 0; i < length; i++) {
+		// move to the next line if newline char is found
+		if (str[i] == '\n') {
+			fb.pos = ((fb.pos / 80) + 1) * 80;
+		}
+		else {
+			fb_putchar(str[i]);
+		}
+	}
+}
--- src/framebuffer.h
+++ src/framebuffer.h
@@ -4,7 +4,7 @@
 #define _FRAMEBUFFER_H_
 
 #include <ints.h>
-#include <string.h>
+#include <defs.h>
 
 #include "ioport.h"
 
@@ -43,40 +43,9 @@ typedef enum {
 	FB_WHITE    = 15
 } fb_color_t;
 
-// Move cursor to a new position
-inline void fb_mov(fb_pos_t pos) {
-	io_out(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-	io_out(FB_DATA_PORT, pos >> 8);
-	io_out(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-	io_out(FB_DATA_PORT, pos & 0x00FF);
-}
-
-// Creates a framebuffer cell based on two colors and an ascii value
-inline fb_char_t fb_mkchar(fb_color_t fb_bgcolor, fb_color_t fb_fgcolor, uint8_t ascii) {
-	return ((uint8_t) fb_bgcolor << 12) | ((uint8_t) fb_fgcolor << 8) | ascii;
-}
-
-// Write a character to the framebuffer
-inline void fb_putchar(uint8_t ascii) {
-	fb.p[fb.pos] = fb_mkchar(FB_BLACK, FB_WHITE, ascii);
-	fb.pos++;
-	fb_mov(fb.pos);
-}
-
-// Write a string to the framebuffer
-inline void fb_puts(const char* str) {
-	size_t i;
-	size_t length = strlen(str);
-
-	for (i = 0; i < length; i++) {
-		// move to the next line if newline char is found
-		if (str[i] == '\n') {
-			fb.pos = ((fb.pos / 80) + 1) * 80;
-		}
-		else {
-			fb_putchar(str[i]);
-		}
-	}
-}
+void fb_mov(fb_pos_t pos);
+fb_char_t fb_mkchar(fb_color_t fb_bgcolor, fb_color_t fb_fgcolor, uint8_t ascii);
+void fb_putchar(uint8_t ascii);
+void fb_puts(const char* str);
 
 #endif
